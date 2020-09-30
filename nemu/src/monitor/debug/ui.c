165f2bb785fab6514bc5c1dfddc3b168e3099355
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 16:35:45 +0800

    >  compile NEMU 3019244252 mac Linux ubuntu 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:35:45 up 1 day,  6:48,  1 user,  load average: 0.25, 0.11, 0.12 f48cc6cb2f9ed981fbfe7c3a781e0db16f8f1d7

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -79,26 +79,6 @@ static int cmd_help(char *args) {
 	return 0;
 }
 
-
-
-
-
-
-
-static int cmd_si(char *args){   
-    char *arg = strtok(args," ");
-    // printf("%s\n",arg);
-    if(arg == NULL){
-        printf("too few arguments.\n");
-        return 1;
-    }
-    int num = atoi(arg);
-    cpu_exec(num);
-    printf("OK");
-    return 0;
-};
-
-/*
 // my code: cmd_si[N]
 static int cmd_si(char *args) {
 	char *arg = strtok(args, " "); // it should be a string of number, such as 1 or 2
@@ -115,7 +95,7 @@ static int cmd_si(char *args) {
 	// printf("test: the number is:%d\n",count_number);
 	return 0;
 }
-*/
+
 void ui_mainloop() {
 	while(1) {
 		char *str = rl_gets();
