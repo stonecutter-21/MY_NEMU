5406065ae1a8445a0262ede5157a2abb6f478c38
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 08:58:45 +0800

    >  compile NEMU 3019244252 mac Linux ubuntu 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:58:45 up 1 day, 19:10,  1 user,  load average: 0.13, 0.16, 0.19 40f56e6b6a2d909e943609882fc4b93ceed845a

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -159,7 +159,7 @@ static int cmd_EXPR(char *args) {
 	bool success=true;
 	int answer = expr (args, &success);
 	if (!success || answer == -1) {
-		// printf ("return == %d\n",answer);
+	    printf ("return == %d\n",answer);
 		printf ("Error input for an expression. Try again\n");
 	}
 	else {
