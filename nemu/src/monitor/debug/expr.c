d1197c517eed1f67990defd05f7e8e83b20747db
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 10:46:43 +0800

    >  compile NEMU 3019244252 mac Linux ubuntu 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:46:43 up 1 day, 20:58,  1 user,  load average: 0.49, 0.31, 0.22 9ea35a62979535a7807bd24d902ffc5ff1677fc

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -288,15 +288,12 @@ uint32_t expr(char *e, bool *success) {
 		*success = false;
 		return 0;
 	}
-	/*
 	int i;
 	for (i = 0 ; i < nr_token; i++) {
-		if 
-		(tokens[i].type == '*' && (i == 0 || (tokens[i].type !=NUMBER && tokens[i].type != ')' ))) {
+		if (tokens[i].type == '*' && (i == 0 || (tokens[i].type !=NUMBER && tokens[i].type != ')' ))) {
 			tokens[i].type = DEREF;
 		}
 	}
-	*/
 	/* TODO: Insert codes to evaluate the expression. */
 	return eval(0, nr_token-1, success); // call this recursive funcition to compute the answer
 	panic("please implement me");
