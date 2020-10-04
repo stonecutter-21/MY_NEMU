37ee4442bc700de6352b84f66c01f1b24ac610e8
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 09:34:10 +0800

    >  compile NEMU 3019244252 mac Linux ubuntu 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:34:10 up 1 day, 19:45,  1 user,  load average: 0.25, 0.17, 0.12 1c86dc7b2cab239e213447be938dd29edc8422a

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -241,7 +241,7 @@ uint32_t eval(int p, int q, bool *success) {
 		return eval (p+1, q-1, success);
 	}
 	// specially deal with the case like '-1'  or '- (1+1)'
-	else if (tokens[p].type == '-' && tokens[p+1].type != '(') {
+	else if (tokens[p].type == '-' && tokens[p+1].type == '(') {
 		return -(eval(p+1,q,success));
 	}
 	else {
