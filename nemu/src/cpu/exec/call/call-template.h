3539e446fce571d85911abd276bb85304f4c9b1a
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-22 21:01:39 +0800

    >  compile NEMU 3019244252 mac Linux ubuntu 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:01:39 up 6 days, 18:54,  1 user,  load average: 0.36, 0.30, 0.27 eff6a7a9e73f4b152b08b5053fade2a20d0d3b6

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -4,18 +4,14 @@
 
 // call immediate number
 make_helper (concat( call_i_, SUFFIX)) {
-    // first we decode and get the length of the instr;
     int length = concat (decode_i_, SUFFIX)(eip);
+    reg_l (R_ESP) -= DATA_BYTE;
     swaddr_write (reg_l (R_ESP), 4, cpu.eip + length);
     DATA_TYPE_S dis = op_src->val; 
-    // then we deal with the esp
-    reg_l (R_ESP) -= DATA_BYTE;
-    // then we write the data into stack
-    
     print_asm("call %x",cpu.eip + 1 + length + dis);
-    printf("eip == %x  ",cpu.eip);
+    // printf("eip == %x  ",cpu.eip);
     
-     printf("dis ==  %x",dis);
+    // printf("dis ==  %x",dis);
     cpu.eip += dis;
     return (length+1);
 }
