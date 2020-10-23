8532fac89d3b83f6340aacff3e097d763b42f188
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-23 08:16:50 +0800

    >  compile NEMU 3019244252 mac Linux ubuntu 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:16:50 up 6 days, 19:57,  1 user,  load average: 1.06, 0.35, 0.22 1a2790313cd8a30bfe6c680ca180b247ac400a7

--- nemu/src/cpu/exec/comp/cmp-template.h
+++ nemu/src/cpu/exec/comp/cmp-template.h
@@ -5,24 +5,19 @@
 static void do_execute() {
 	DATA_TYPE answer = op_dest->val - op_src->val;
 	int length = (DATA_BYTE << 3) - 1;
-    bool flg = 0;
-    if (op_dest->val < op_src->val) {
-        flg = 1;
-    }
-	cpu.CF = flg;
+	cpu.CF = op_dest->val < op_src->val;
 	cpu.SF=answer >> length;
     int s1,s2;
-	s1 = op_dest->val >> length;
-	s2 = op_src->val >> length;
-    cpu.OF = (s1 != s2 && s2 == cpu.SF) ;
-    cpu.ZF =! answer;
+	s1=op_dest->val>>length;
+	s2=op_src->val>>length;
+    cpu.OF=(s1 != s2 && s2 == cpu.SF) ;
+    cpu.ZF=!answer;
 	answer ^= answer >>4;
 	answer ^= answer >>2;
 	answer ^= answer >>1;
-	cpu.PF = !(answer & 1);
+	cpu.PF=!(answer & 1);
 	print_asm("cmp %s,%s", op_src->str, op_dest->str);
 }
-
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 make_instr_helper(si2rm)
 #endif
@@ -31,5 +26,6 @@ make_instr_helper(i2a)
 make_instr_helper(i2rm)
 make_instr_helper(r2rm)
 make_instr_helper(rm2r)
+
 
 #include "cpu/exec/template-end.h"
