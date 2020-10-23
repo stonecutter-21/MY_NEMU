8bd2dd5135c0136395a904a487fc8eedb3e80eee
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-23 08:41:01 +0800

    >  compile NEMU 3019244252 mac Linux ubuntu 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:41:01 up 6 days, 20:21,  1 user,  load average: 0.14, 0.19, 0.18 be8bd7d560c45a1bb29b1e50442ceffcbfb91920

--- nemu/src/cpu/exec/comp/cmp-template.h
+++ nemu/src/cpu/exec/comp/cmp-template.h
@@ -3,7 +3,6 @@
 #define instr cmp
 
 static void do_execute() {
-	printf("here?\n");
 	DATA_TYPE answer = op_dest->val - op_src->val;
 	int length = (DATA_BYTE << 3) - 1;
 	cpu.CF = (op_dest->val < op_src->val);
