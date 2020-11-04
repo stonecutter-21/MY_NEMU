1f4527255dbfdea3e11f8683676f94081348e005
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-04 21:12:15 +0800

    >  compile NEMU 3019244252 mac Linux ubuntu 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:12:15 up 22:46,  1 user,  load average: 0.18, 0.13, 0.17 197d1f5f8f8ac85a197b1de0b3c2635ca13ac83

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -91,6 +91,7 @@ uint32_t get_value_of_symbol(char* str,bool* success){
 	for (i = 0; i < nr_symtab_entry; i++){
 		if ((symtab[i].st_info & 0xf) == STT_OBJECT){ // when find it
 			char temp[32];
+			memset(temp,0,32);
 			int len = strlen(str);
 			strncpy(temp,strtab + symtab[i].st_name,len);
 			printf("temp == %s\n",temp);
