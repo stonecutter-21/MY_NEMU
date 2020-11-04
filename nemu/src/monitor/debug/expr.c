16f210f825932ee4b22d31e35deb3b42086b5727
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-04 21:46:57 +0800

    >  compile NEMU 3019244252 mac Linux ubuntu 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:46:57 up 23:21,  1 user,  load average: 0.12, 0.18, 0.18 5c08d07c642dc62841fd9555f45226842192c9bb

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -358,7 +358,7 @@ uint32_t eval(int p, int q, bool *success, int *format) {
 	}
 
 	int index = dominant_operator(p, q);
-	//printf ("index == %d\n", index);
+	printf ("index == %d\n", index);
 	//printf ("p == %d  ", p);
 	//printf ("q == %d  ", q);
 	int op = tokens[index].type;
