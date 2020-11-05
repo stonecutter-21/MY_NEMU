6fa9aca5870d927cf232dc84a9df1f21c50b481b
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-05 20:39:06 +0800

    >  compile NEMU 3019244252 mac Linux ubuntu 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:39:06 up 1 day,  3:11,  1 user,  load average: 0.25, 0.10, 0.13 7efcb96f59557af7618862338214512c4e551d18

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -38,7 +38,7 @@ uint32_t loader() {
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	// panic("please implement me");
+	 panic("please implement me");
 	for(i = 0;i < elf->e_phnum; i++) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
